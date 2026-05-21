2cfdc234dd08bc9a789715843b90514d52d25404
lucas martins de abreu <lmdamartins31@gmail.com>
2026-05-21 16:09:10 -0300

    interface_usuario

---
+++ exemplos/exemplo_bits.py
@@ -0,0 +1,4 @@
+//Deve demonstrar a leitura dos 16 bits de um registrador e a alteração de um bit específico.
+
+
+
---
+++ exemplos/exemplo_float.py
@@ -0,0 +1,4 @@
+//Deve demonstrar a escrita e leitura de um valor float utilizando Holding Registers.
+
+
+
