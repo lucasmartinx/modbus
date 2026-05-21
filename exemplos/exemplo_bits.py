e3e3997130f436fe0265037e9c3b296d0b7edb03
lucas martins de abreu <lmdamartins31@gmail.com>
2026-05-21 17:18:53 -0300

    projeto finalizado

--- exemplos/exemplo_bits.py
+++ exemplos/exemplo_bits.py
@@ -2,3 +2,29 @@
 
 
 
+from clientemodbus import ClienteMODBUS
+from time import sleep
+
+# Cria o cliente apontando para o seu servidor
+cliente = ClienteMODBUS('localhost', 5020)
+
+print("--- TESTE DE MANIPULAÇÃO DE BITS ---")
+cliente.conectar()
+
+endereco = 20  # Endereço do Holding Register que vamos usar
+bit_alvo = 3   # Vamos alterar o bit 3 (lembrando que vai de 0 a 15)
+estado = 1     # 1 para ligar, 0 para desligar
+
+# Escreve o Bit
+print(f"Ligando o bit {bit_alvo} do registrador {endereco}...")
+sucesso = cliente.escreveBitRegistrador(endereco, bit_alvo, estado)
+
+if sucesso:
+    sleep(0.5) # Pausa rápida
+    # Lê todos os 16 bits do registrador para ver como ficou
+    bits_lidos = cliente.lerBitsRegistrador(endereco)
+    print(f"Leitura dos 16 bits (do 0 ao 15): {bits_lidos}")
+else:
+    print("Falha ao tentar alterar o bit.")
+
+cliente.fechar()
--- exemplos/exemplo_float.py
+++ exemplos/exemplo_float.py
@@ -2,3 +2,28 @@
 
 
 
+from clientemodbus import ClienteMODBUS
+from time import sleep
+
+# Cria o cliente apontando para o seu servidor
+cliente = ClienteMODBUS('localhost', 5020)
+
+print("--- TESTE DE FLOAT (32 bits) ---")
+cliente.conectar()
+
+endereco = 10  # Escolha um endereço inicial da sua tabela
+valor_para_escrever = 3.1415
+
+# Escreve o Float
+print(f"Escrevendo o valor {valor_para_escrever} no endereço {endereco}...")
+sucesso = cliente.escreveFloat(endereco, valor_para_escrever)
+
+if sucesso:
+    sleep(0.5) # Pausa rápida para o servidor processar
+    # Lê o Float de volta
+    valor_lido = cliente.lerFloat(endereco)
+    print(f"Leitura concluída! Valor lido: {valor_lido}")
+else:
+    print("Falha ao tentar escrever o Float.")
+
+cliente.fechar()
